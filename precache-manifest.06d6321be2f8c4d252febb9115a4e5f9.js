11734036b74b2c4b7b6fb902e337624cdcd3fa2d
Artyom Bochkov <art-bochkov@yandex-team.ru>
2022-01-12 04:28:15 +0300

    Updates

---
+++ precache-manifest.06d6321be2f8c4d252febb9115a4e5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ed7669256b52beb68fd368897b358e",
+    "url": "/rating-foos-admin/index.html"
+  },
+  {
+    "revision": "f91c3398ec2083fd5e10",
+    "url": "/rating-foos-admin/static/css/2.d7feca41.chunk.css"
+  },
+  {
+    "revision": "074596e0fd0d258e69c2",
+    "url": "/rating-foos-admin/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f91c3398ec2083fd5e10",
+    "url": "/rating-foos-admin/static/js/2.b14d5d23.chunk.js"
+  },
+  {
+    "revision": "074596e0fd0d258e69c2",
+    "url": "/rating-foos-admin/static/js/main.3c2f4be7.chunk.js"
+  },
+  {
+    "revision": "6b97d7b69cf8321e4422",
+    "url": "/rating-foos-admin/static/js/runtime~main.d7e9004e.js"
+  }
+]);
