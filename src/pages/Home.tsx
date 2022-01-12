548571d4f8de0ad395a0f2eec9dc4de60d240de9
Artyom Bochkov <art-bochkov@yandex-team.ru>
2022-01-12 04:48:32 +0300

    Add download button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 
 const Home = () => {
     return (
         <>
-            <Button type="primary">Home</Button>
+            <Button type="dashed" shape="round" icon={<DownloadOutlined />}>
+                Download fast file
+            </Button>
             <nav>
                 <Link to="/players">Players</Link>
             </nav>
